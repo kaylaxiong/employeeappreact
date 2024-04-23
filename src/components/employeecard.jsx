ff0b4b4be68a6314ee195343af115fda7aaee108
kaylaxiong <159079556+kaylaxiong@users.noreply.github.com>
2024-04-22 22:42:12 -0400

    5 employee card

--- src/components/employeecard.jsx
+++ src/components/employeecard.jsx
@@ -9,37 +9,41 @@ import AppleIcon from '@mui/icons-material/Apple';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
-export default function MediaCard({employee}) {
+
+export default function MediaCard({ employee }) {
   console.log(employee);
   return (
-    <Card sx={{ maxWidth: 345, margin:10, padding:2}}>
-      <CardMedia
-        sx={{ height: 280, backgroundSize: 'contain', margin:1} }
-        image="/profile1.png"
-        title="green iguana"
-      />
-      <CardContent>
-      <AppleIcon />
-        <Typography gutterBottom variant="h5" component="div">
-      {employee[0].firstname}{employee[0].lastname}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {employee[0].department}
-        </Typography>
-        <Typography>
-          {employee[0].position}
-        </Typography>
-      </CardContent>
+    <>
+      {employee.map((item, index) => (
+        <Card sx={{ maxWidth: 345, margin: 10, padding: 1 }}>
+          <CardMedia
+            sx={{ height: 200, backgroundSize: "contain", margin: 1 }}
+            image={`/employeeappreact/${item.image}`}
+            title="profile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.firstName} {item.lastName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.department}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.position}
+            </Typography>
+          </CardContent>
 
-      <Stack direction="row" spacing={1}>
-      <Chip label="Chip Filled" />
-      <Chip label="Chip Outlined" variant="outlined" />
-    </Stack>
+          <Stack direction="row" spacing={1}>
+            <Chip label="Chip Filled" />
+            <Chip label="Chip Outlined" variant="outlined" />
+          </Stack>
 
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </>
   );
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,6 @@
 const data = [{
     "id": "E1002",
     "image":"profile1.png",
-    "image":"profile2.png",
-    "image":"profile3.png",
-    "image":"profile4.png",
-    "image":"profile5.png",
     "firstName": "John",
     "lastName": "Smith",
     "email": "john.smith@example.com",
@@ -26,7 +22,8 @@ const data = [{
     "employmentType": "Remote",
     "skills": ["SEO", "Content Marketing", "Google Analytics"],
     "hoursLoggedThisWeek": 35,
-    "onLeave": false
+    "onLeave": false,
+    "image":"profile2.png",
   },
   
   {
@@ -39,7 +36,8 @@ const data = [{
     "employmentType": "Full-Time",
     "skills": ["Node.js", "MongoDB", "Express"],
     "hoursLoggedThisWeek": 40,
-    "onLeave": true
+    "onLeave": true,
+    "image":"profile3.png",
   },
   
   {
@@ -52,7 +50,8 @@ const data = [{
     "employmentType": "Full-Time",
     "skills": ["Recruitment", "Employee Relations", "Performance Management"],
     "hoursLoggedThisWeek": 40,
-    "onLeave": false
+    "onLeave": false,
+    "image":"profile4.png",
   },
   
   {
@@ -65,7 +64,8 @@ const data = [{
     "employmentType": "Full-Time",
     "skills": ["Network Security", "Hardware Troubleshooting", "Customer Service"],
     "hoursLoggedThisWeek": 40,
-    "onLeave": false
+    "onLeave": false,
+    "image":"profile5.png",
   }]
 
   export default data; 
